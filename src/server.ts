7c3cfddacd3dba8b071e9b34df4d25d3e8f3aadf
enes.sefa.k <enes.sefa.k@gmail.com>
2020-04-10 14:36:38 +0300

    change stream

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,52 @@
+import hyperquest from 'hyperquest'
+import through from 'through2'
 import dotenv from 'dotenv'
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
-import { getContents } from './utils'
+// import { getContents } from './utils'
 
 dotenv.config()
 
 const PORT = process.env.PORT || 80
 const app: Application = express()
 
+app.set('views', 'views')
+app.set('view engine', 'pug')
+
 app.use(bodyParser.json())
 
 app.use(cors())
 
-app.get('/', async (req: Request, res: Response) => {
-  const data = await getContents('http://localhost:81')
-  console.log(data)
-  res.send('deneme')
+app.get('*', async (req: Request, res: Response) => {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
+
+  res.write(`<!DOCTYPE html>
+      <html>
+        <head>
+          <title>Client</title>
+            <script crossorigin src="https://unpkg.com/react@16/umd/react.development.js"></script>
+            <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
+            <script crossorigin src="https://unpkg.com/react-router/umd/react-router.min.js"></script>
+            <script crossorigin src="https://unpkg.com/react-router-dom/umd/react-router-dom.min.js"></script>
+        </head>
+        <body>`)
+
+  const write = (row, enc, next) => {
+    const data = JSON.parse(String(row))
+    data.html && res.write(data.html)
+    data.script && res.write(data.script)
+    next()
+    // next(null, String(row.value * row.value) + '\n')
+  }
+
+  const r = hyperquest('http://localhost:81')
+  r.pipe(through.obj(write))
+
+  r.on('end', function () {
+    res.write(`</body></html>`)
+    res.end()
+  })
 })
 
 app.listen(PORT, () => {
