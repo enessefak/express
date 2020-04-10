12e8cbd4da68681c339cfb8823432c4a500dfcfa
enes.sefa.k <enes.sefa.k@gmail.com>
2020-04-10 16:14:51 +0300

    production

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import dotenv from 'dotenv'
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import compression from 'compression'
 import bodyParser from 'body-parser'
 import hyperquest from 'hyperquest'
 import through from 'through2'
 import pump from 'pump'
-// import { getContents } from './utils'
 
 dotenv.config()
 
@@ -16,6 +16,8 @@ app.set('views', 'views')
 app.set('view engine', 'pug')
 
 app.use(bodyParser.json())
+
+app.use(compression())
 
 app.use(cors())
 
@@ -30,16 +32,32 @@ app.get('*', async (req: Request, res: Response) => {
         <body>`)
 
   const write = (row, enc, next) => {
-    const data = JSON.parse(String(row))
-    data.html && res.write(data.html)
-    // data.script && res.write(data.script)
+    try {
+      const data = JSON.parse(String(row))
+      data.html && res.write(data.html)
+      data.script &&
+        res.write(`<script crossorigin="crossorigin" src="https://unpkg.com/react@16/umd/react.development.js"></script>
+      <script crossorigin="crossorigin" src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
+      <script crossorigin="crossorigin" src="https://unpkg.com/react-router/umd/react-router.min.js"></script>
+      <script crossorigin="crossorigin" src="https://unpkg.com/react-router-dom/umd/react-router-dom.min.js"></script>
+      ${data.script}`)
+    } catch (error) {
+      console.error(error)
+    }
+
     next()
   }
 
-  pump(hyperquest('http://localhost:81'), through(write), () => {
-    res.write(`</body></html>`)
-    res.end()
-  })
+  const fragment = [
+    hyperquest(`http://localhost:81${req.url}`),
+    through(write),
+    () => {
+      res.write(`</body></html>`)
+      res.end()
+    }
+  ]
+
+  pump(...fragment)
 })
 
 app.listen(PORT, () => {
