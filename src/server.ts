3dc7520448d56b12a1868fa62b54f71917f1f3d3
enes.sefa.k <enes.sefa.k@gmail.com>
2020-04-10 15:22:22 +0300

    add packages

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
-import hyperquest from 'hyperquest'
-import through from 'through2'
 import dotenv from 'dotenv'
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import hyperquest from 'hyperquest'
+import through from 'through2'
+import pump from 'pump'
 // import { getContents } from './utils'
 
 dotenv.config()
@@ -25,25 +26,17 @@ app.get('*', async (req: Request, res: Response) => {
       <html>
         <head>
           <title>Client</title>
-            <script crossorigin src="https://unpkg.com/react@16/umd/react.development.js"></script>
-            <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
-            <script crossorigin src="https://unpkg.com/react-router/umd/react-router.min.js"></script>
-            <script crossorigin src="https://unpkg.com/react-router-dom/umd/react-router-dom.min.js"></script>
         </head>
         <body>`)
 
   const write = (row, enc, next) => {
     const data = JSON.parse(String(row))
     data.html && res.write(data.html)
-    data.script && res.write(data.script)
+    // data.script && res.write(data.script)
     next()
-    // next(null, String(row.value * row.value) + '\n')
   }
 
-  const r = hyperquest('http://localhost:81')
-  r.pipe(through.obj(write))
-
-  r.on('end', function () {
+  pump(hyperquest('http://localhost:81'), through(write), () => {
     res.write(`</body></html>`)
     res.end()
   })
